859fde3a66215e6a0127ce9e87f1489caf63fb7c
Georgy <gogaopel@gmail.com>
2015-12-07 22:51:34 +0300

    Заcчитано бонус 8 2

--- src/com/javarush/test/level08/lesson11/bonus02/Solution.java
+++ src/com/javarush/test/level08/lesson11/bonus02/Solution.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /* Нужно добавить в программу новую функциональность
@@ -30,21 +31,23 @@ public class Solution
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         //list of addresses
-        List<String> addresses = new ArrayList<String>();
+        HashMap<String, String> addresses = new HashMap<>();
         while (true)
         {
+            String city = reader.readLine();
+            if (city.isEmpty()) break;
             String family = reader.readLine();
             if (family.isEmpty()) break;
 
-            addresses.add(family);
+            addresses.put(city,family);
         }
 
         //read home number
-        int houseNumber = Integer.parseInt(reader.readLine());
-
-        if (0 <= houseNumber && houseNumber < addresses.size())
+        //int houseNumber = Integer.parseInt(reader.readLine());
+        String city = reader.readLine();
+        if (!city.isEmpty())
         {
-            String familySecondName = addresses.get(houseNumber);
+            String familySecondName = addresses.get(city) ;
             System.out.println(familySecondName);
         }
     }
