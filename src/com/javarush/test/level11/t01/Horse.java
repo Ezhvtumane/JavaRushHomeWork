4e1083208268da374672b8d29c18ef18e3e03e73
Georgy <gogaopel@gmail.com>
2015-12-09 22:01:34 +0300

    New branch for free

---
+++ src/com/javarush/test/level11/t01/Horse.java
@@ -0,0 +1,8 @@
+package com.javarush.test.level11.t01;
+
+/**
+ * Created by georgy on 09.12.15.
+ */
+public class Horse
+{
+}
---
+++ src/com/javarush/test/level11/t01/Pegas.java
@@ -0,0 +1,10 @@
+package com.javarush.test.level11.t01;
+
+import com.javarush.test.level11.t01.Horse;
+
+/**
+ * Created by georgy on 09.12.15.
+ */
+public class Pegas extends Horse
+{
+}
