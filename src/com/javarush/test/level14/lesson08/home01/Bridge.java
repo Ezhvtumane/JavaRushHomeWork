e7929eecde8e268f9fe0a273f0812a22c60a57e5
georgy-lu <gogaopel@gmail.com>
2015-12-23 21:59:35 +0300

    Level 14 lesson 08 home01

---
+++ src/com/javarush/test/level14/lesson08/home01/Bridge.java
@@ -0,0 +1,9 @@
+package com.javarush.test.level14.lesson08.home01;
+
+/**
+ * Created by lubuntu-v on 23.12.15.
+ */
+public interface Bridge
+{
+    int getCarsCount();
+}
---
+++ src/com/javarush/test/level14/lesson08/home01/WaterBridge.java
@@ -0,0 +1,12 @@
+package com.javarush.test.level14.lesson08.home01;
+
+/**
+ * Created by lubuntu-v on 23.12.15.
+ */
+public class WaterBridge implements Bridge{
+    @Override
+    public int getCarsCount()
+    {
+        return 20;
+    }
+}
