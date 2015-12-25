ccdac8e83ce8583c3784fdf608920920d12d1d0a
georgy-lu <gogaopel@gmail.com>
2015-12-25 21:57:03 +0300

    Level 14 lesson 08 home06 after

--- src/com/javarush/test/level14/lesson08/home06/Solution.java
+++ src/com/javarush/test/level14/lesson08/home06/Solution.java
@@ -17,18 +17,30 @@ package com.javarush.test.level14.lesson08.home06;
 8.2. Вывести на экран movie.getClass().getSimpleName().
 */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class Solution
 {
     public static void main(String[] args) throws Exception
     {
-        //ввести с консоли несколько ключей (строк), пункт 7
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
+        String key=reader.readLine();
+        while(key.equals("cartoon") || key.equals("thriller") || key.equals("soapOpera"))
+        {
+            list.add(key);
+            key=reader.readLine();
+        }
 
-        /*
-8 Создать переменную movie класса Movie и для каждой введенной строки(ключа):
-8.1 получить объект используя MovieFactory.getMovie и присвоить его переменной movie
-8.2 вывести на экран movie.getClass().getSimpleName()
-        */
+        Movie movie = null;
 
+        for (int i = 0; i < list.size(); i++)
+        {
+            movie = MovieFactory.getMovie(list.get(i));
+            System.out.println(movie.getClass().getSimpleName());
+        }
     }
 
     static class MovieFactory
@@ -37,15 +49,18 @@ public class Solution
         static Movie getMovie(String key)
         {
             Movie movie = null;
-
-            //создание объекта SoapOpera (мыльная опера) для ключа "soapOpera"
             if ("soapOpera".equals(key))
             {
                 movie = new SoapOpera();
             }
-
-            //напишите тут ваш код, пункты 5,6
-
+            else if ("cartoon".equals(key))
+            {
+                movie = new Cartoon();
+            }
+            else if ("thriller".equals(key))
+            {
+                movie = new Thriller();
+            }
             return movie;
         }
     }
@@ -58,5 +73,13 @@ public class Solution
     {
     }
 
-    //Напишите тут ваши классы, пункт 3
+    static class Cartoon extends Movie
+    {
+
+    }
+
+    static class Thriller extends Movie
+    {
+
+    }
 }
