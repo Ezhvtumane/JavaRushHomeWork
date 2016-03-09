7cf34a3670eb3df41dec66e1184a8bfd00306087
Ezhvtumane <gogaopel@gmail.com>
2016-03-09 14:50:56 +0300

    WinWrk l16.l13.h06 after h07 before

--- src/com/javarush/test/level16/lesson13/home06/Solution.java
+++ src/com/javarush/test/level16/lesson13/home06/Solution.java
@@ -14,8 +14,29 @@ public class Solution {
     }
 
     //Add your code below - добавьте код ниже
-    public static class CountUpRunnable {
+    public static class CountUpRunnable implements Runnable {
+        private int countIndexUp = 1;
+        private Thread t;
+
         public CountUpRunnable(String name) {
+            t = new Thread(this,name);
+            t.start();
+        }
+
+        public void run() {
+            try {
+                while (true) {
+                    System.out.println(toString());
+                    countIndexUp += 1;
+                    if (countIndexUp > Solution.number) return;
+                    Thread.sleep(500);
+                }
+            } catch (InterruptedException e) {
+            }
+        }
+
+        public String toString() {
+            return t.getName() + ": " + countIndexUp;
         }
     }
 
