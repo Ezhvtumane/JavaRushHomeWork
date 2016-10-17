6b3c458b9d404e9dba85fcf9c43784054318ce38
Georgy <gogaopel@gmail.com>
2016-10-17 19:46:15 +0300

    Level 18 lesson 03 task 01 fin task 02 start

--- src/com/javarush/test/level18/lesson03/task01/Solution.java
+++ src/com/javarush/test/level18/lesson03/task01/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.test.level18.lesson03.task01;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 /* Максимальный байт
 Ввести с консоли имя файла
@@ -10,5 +12,25 @@ import java.io.FileInputStream;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String fileName = reader.readLine();
+
+        FileInputStream inputStream = new FileInputStream(fileName);
+
+        int k=0,a;
+
+        while(inputStream.available()>0){
+            if(k<(a=inputStream.read())) k=a;
+        }
+
+
+        System.out.println(k);
+
+        inputStream.close();
+
+
+
     }
 }
